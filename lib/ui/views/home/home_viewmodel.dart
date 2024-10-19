c244d01b811852861e753cc50e23702f173e46ad
Amit Patil <amit@gocopious.com>
2024-10-19 18:29:45 +0530

    inc

---
+++ lib/ui/views/home/home_viewmodel.dart
@@ -0,0 +1,40 @@
+
+import 'package:rss/app/app.bottomsheets.dart';
+import 'package:rss/app/app.dialogs.dart';
+import 'package:rss/app/app.locator.dart';
+import 'package:rss/ui/common/app_strings.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class HomeViewModel extends BaseViewModel {
+  final _dialogService = locator<DialogService>();
+  final _bottomSheetService = locator<BottomSheetService>();
+
+  String get counterLabel => 'Counter is: $_counter';
+
+  int _counter = 0;
+
+  void incrementCounter() async {
+    // await FirebaseFirestore.instance
+    //     .collection('user')
+    //     .add(User(name: "as", age: 12, email: "email"));
+    _counter++;
+    rebuildUi();
+  }
+
+  void showDialog() {
+    _dialogService.showCustomDialog(
+      variant: DialogType.infoAlert,
+      title: 'Stacked Rocks!',
+      description: 'Give stacked $_counter stars on Github',
+    );
+  }
+
+  void showBottomSheet() {
+    _bottomSheetService.showCustomSheet(
+      variant: BottomSheetType.notice,
+      title: ksHomeBottomSheetTitle,
+      description: ksHomeBottomSheetDescription,
+    );
+  }
+}
