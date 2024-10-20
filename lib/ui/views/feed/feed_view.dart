fd320f95f1d40a63bd26c0fb70aadd8b54a99df2
Amit Patil <amit@gocopious.com>
2024-10-20 13:44:34 +0530

    opacity change

--- lib/ui/views/feed/feed_view.dart
+++ lib/ui/views/feed/feed_view.dart
@@ -21,7 +21,7 @@ class FeedView extends StackedView<FeedViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Colors.white.withOpacity(0.95),
+      backgroundColor: Colors.white.withOpacity(0.93),
       body: Container(
         margin: const EdgeInsets.only(left: 5.0, right: 5.0),
         padding: const EdgeInsets.only(left: 5.0, right: 5.0),
@@ -78,169 +78,180 @@ class FeedView extends StackedView<FeedViewModel> {
                 query: viewModel.getQuery(),
                 itemBuilder: (context, snapshot) {
                   PostsModel postModel = snapshot.data();
-                  return Slidable(
-                    // Specify a key if the Slidable is dismissible.
-                    key: ValueKey(postModel.id),
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 20.0),
+                    child: Slidable(
+                      // Specify a key if the Slidable is dismissible.
+                      key: ValueKey(postModel.id),
 
-                    // The start action pane is the one at the left or the top side.
-                    startActionPane: ActionPane(
-                      // A motion is a widget used to control how the pane animates.
-                      motion: const ScrollMotion(),
+                      // The start action pane is the one at the left or the top side.
+                      startActionPane: ActionPane(
+                        // A motion is a widget used to control how the pane animates.
+                        motion: const ScrollMotion(),
 
-                      // A pane can dismiss the Slidable.
-                      dismissible: DismissiblePane(onDismissed: () {}),
-                      dragDismissible: false,
-                      // All actions are defined in the children parameter.
-                      children: [
-                        // A SlidableAction can have an icon and/or a label.
-                        SlidableAction(
-                          onPressed: (context) =>
-                              viewModel.priortiseTask(postModel),
-                          backgroundColor: Colors.purpleAccent,
-                          foregroundColor: Colors.white,
-                          icon: Icons.bolt_rounded,
-                          label: 'Priortise',
-                        ),
-                        SlidableAction(
-                          onPressed: (s) => viewModel.completeTask(postModel),
-                          backgroundColor: Colors.greenAccent,
-                          foregroundColor: Colors.white,
-                          icon: Icons.check,
-                          label: 'Complete',
-                        ),
-                      ],
-                    ),
-
-                    // The end action pane is the one at the right or the bottom side.
-                    endActionPane: ActionPane(
-                      motion: const ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          onPressed: (s) => viewModel.updateLastSeen(postModel),
-                          backgroundColor: Colors.purple,
-                          foregroundColor: Colors.white,
-                          icon: Icons.spa_rounded,
-                          label: 'Seen',
-                        ),
-                      ],
-                    ),
-
-                    // The child of the Slidable is what the user sees when the
-                    // component is not dragged.
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 15.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Row(
+                        // A pane can dismiss the Slidable.
+                        dismissible: DismissiblePane(onDismissed: () {}),
+                        dragDismissible: false,
+                        // All actions are defined in the children parameter.
                         children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                if (postModel.url != null) ...[
-                                  AnyLinkPreview(
-                                    removeElevation: true,
-                                    previewHeight:
-                                        MediaQuery.of(context).size.width * 0.5,
-                                    link: postModel.url!,
-                                    cache: const Duration(hours: 5),
-                                    borderRadius: 10,
-                                    backgroundColor: Colors.white,
-                                    errorWidget: Container(
-                                      color: Colors.white,
-                                      child: const Text("Couldn't get preview"),
-                                    ),
-                                  ),
-                                  const Divider(
-                                    thickness: 0.1,
-                                  ),
-                                ],
-                                if (postModel.url == null) ...[
-                                  verticalSpaceSmall,
-                                ],
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0),
-                                  child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [
-                                            if (postModel.url != null)
-                                              const Icon(Icons.video_label_rounded,
-                                                  size: 15),
-                                            if (postModel.url == null)
-                                              const Icon(Icons.track_changes_outlined,
-                                                  size: 15),
-                                            horizontalSpaceTiny,
-                                            Expanded(
-                                              child: ReadMoreText(
-                                                postModel.text ?? '',
-                                                trimMode: TrimMode.Line,
-                                                trimLines: 3,
-                                                colorClickableText: Colors.pink,
-                                                trimCollapsedText: 'Show more',
-                                                trimExpandedText: ' Show less',
-                                                style: AppTextStyles.h6,
-                                                moreStyle: const TextStyle(
-                                                    fontSize: 14,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        ReadMoreText(
-                                          postModel.desc ?? '',
-                                          trimMode: TrimMode.Line,
-                                          trimLines: 3,
-                                          colorClickableText: Colors.pink,
-                                          trimCollapsedText: 'Show more',
-                                          trimExpandedText: ' Show less',
-                                          style: AppTextStyles.bodyText1,
-                                          moreStyle: const TextStyle(
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        verticalSpaceTiny,
-                                        Row(
-                                          children: [
-                                            Text(
-                                              "${DateFormat('dd MMM, hh:MM a').format(
-                                                postModel.lastSeen ??
-                                                    DateTime.now(),
-                                              )} |",
-                                              style: AppTextStyles.caption,
-                                            ),
-                                            horizontalSpaceTiny,
-                                            Text(
-                                              "Due on ${DateFormat('dd MMM').format(
-                                                postModel.dueDate ??
-                                                    DateTime.now(),
-                                              )} |",
-                                              style: AppTextStyles.caption,
-                                            ),
-                                            horizontalSpaceTiny,
-                                            Text(
-                                              "Created on ${DateFormat('dd MMM').format(
-                                                postModel.createdAt ??
-                                                    DateTime.now(),
-                                              )}",
-                                              overflow: TextOverflow.ellipsis,
-                                              style: AppTextStyles.caption,
-                                            ),
-                                          ],
-                                        ),
-                                        verticalSpaceSmall,
-                                      ]),
-                                ),
-                              ],
-                            ),
+                          // A SlidableAction can have an icon and/or a label.
+                          SlidableAction(
+                            onPressed: (context) =>
+                                viewModel.priortiseTask(postModel),
+                            backgroundColor: Colors.purpleAccent,
+                            foregroundColor: Colors.white,
+                            icon: Icons.bolt_rounded,
+                            label: 'Priortise',
+                          ),
+                          SlidableAction(
+                            onPressed: (s) => viewModel.completeTask(postModel),
+                            backgroundColor: Colors.greenAccent,
+                            foregroundColor: Colors.white,
+                            icon: Icons.check,
+                            label: 'Complete',
                           ),
                         ],
+                      ),
+
+                      // The end action pane is the one at the right or the bottom side.
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            onPressed: (s) =>
+                                viewModel.updateLastSeen(postModel),
+                            backgroundColor: Colors.purple,
+                            foregroundColor: Colors.white,
+                            icon: Icons.spa_rounded,
+                            label: 'Seen',
+                          ),
+                        ],
+                      ),
+
+                      // The child of the Slidable is what the user sees when the
+                      // component is not dragged.
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  if (postModel.url != null) ...[
+                                    AnyLinkPreview(
+                                      removeElevation: true,
+                                      previewHeight:
+                                          MediaQuery.of(context).size.width *
+                                              0.5,
+                                      link: postModel.url!,
+                                      cache: const Duration(hours: 5),
+                                      borderRadius: 10,
+                                      backgroundColor: Colors.white,
+                                      errorWidget: Container(
+                                        color: Colors.white,
+                                        child:
+                                            const Text("Couldn't get preview"),
+                                      ),
+                                    ),
+                                    const Divider(
+                                      thickness: 0.1,
+                                    ),
+                                  ],
+                                  if (postModel.url == null) ...[
+                                    verticalSpaceSmall,
+                                  ],
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10.0),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              if (postModel.url != null)
+                                                const Icon(
+                                                    Icons.video_label_rounded,
+                                                    size: 15),
+                                              if (postModel.url == null)
+                                                const Icon(
+                                                    Icons
+                                                        .track_changes_outlined,
+                                                    size: 15),
+                                              horizontalSpaceTiny,
+                                              Expanded(
+                                                child: ReadMoreText(
+                                                  postModel.text ?? '',
+                                                  trimMode: TrimMode.Line,
+                                                  trimLines: 3,
+                                                  colorClickableText:
+                                                      Colors.pink,
+                                                  trimCollapsedText:
+                                                      'Show more',
+                                                  trimExpandedText:
+                                                      ' Show less',
+                                                  style: AppTextStyles.h6,
+                                                  moreStyle: const TextStyle(
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          ReadMoreText(
+                                            postModel.desc ?? '',
+                                            trimMode: TrimMode.Line,
+                                            trimLines: 3,
+                                            colorClickableText: Colors.pink,
+                                            trimCollapsedText: 'Show more',
+                                            trimExpandedText: ' Show less',
+                                            style: AppTextStyles.bodyText1,
+                                            moreStyle: const TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          verticalSpaceTiny,
+                                          Row(
+                                            children: [
+                                              Text(
+                                                "${DateFormat('dd MMM, hh:MM a').format(
+                                                  postModel.lastSeen ??
+                                                      DateTime.now(),
+                                                )} |",
+                                                style: AppTextStyles.caption,
+                                              ),
+                                              horizontalSpaceTiny,
+                                              Text(
+                                                "Due on ${DateFormat('dd MMM').format(
+                                                  postModel.dueDate ??
+                                                      DateTime.now(),
+                                                )} |",
+                                                style: AppTextStyles.caption,
+                                              ),
+                                              horizontalSpaceTiny,
+                                              Text(
+                                                "Created on ${DateFormat('dd MMM').format(
+                                                  postModel.createdAt ??
+                                                      DateTime.now(),
+                                                )}",
+                                                overflow: TextOverflow.ellipsis,
+                                                style: AppTextStyles.caption,
+                                              ),
+                                            ],
+                                          ),
+                                          verticalSpaceSmall,
+                                        ]),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   );
