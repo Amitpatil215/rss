866a7b144188e22965a9fc6ad2276f183ab8ae11
Amit Patil <amit@gocopious.com>
2024-10-20 14:49:32 +0530

    coom

--- lib/ui/views/feed/feed_viewmodel.dart
+++ lib/ui/views/feed/feed_viewmodel.dart
@@ -12,16 +12,6 @@ enum FilterBy { VIDEO, TASK, PENDING, COMPLETED }
 class FeedViewModel extends BaseViewModel {
   final _bottomSheetService = locator<BottomSheetService>();
 
-  // final postsQuery = FirebaseFirestore.instance
-  //     .collection('posts')
-  //     .where('status', isEqualTo: 'pending')
-  //     .orderBy('due_date', descending: false)
-  //     .orderBy('last_seen', descending: false)
-  //     .withConverter<PostsModel>(
-  //       fromFirestore: (snapshot, _) =>
-  //           PostsModel.fromJson(snapshot.data()!, snapshot.reference.id),
-  //       toFirestore: (post, _) => post.toJson(),
-  //     );
   Query<PostsModel> getQuery() {
     /// use filter by for writing the query
     var collectionRef = FirebaseFirestore.instance.collection('posts');
